aa93bc28686ebfa7b7a4b603ba068750bd5f0a9a
xutongle <xutongle@gmail.com>
2013-02-23 10:44:12 +0800

    添加跟文件
    Signed-off-by: xutongle <xutongle@gmail.com>

---
+++ admin.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * 后台入口
+ * @author Tongle Xu <xutongle@gmail.com> 2012-5-26
+ * @copyright Copyright (c) 2003-2103 yuncms.net
+ * @license http://leaps.yuncms.net
+ * @version $Id: admin.php 13 2012-11-05 10:06:54Z xutongle $
+ */
+header ( 'location:index.php?app=admin' );
